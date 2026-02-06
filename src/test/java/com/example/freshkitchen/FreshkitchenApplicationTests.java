9c9b08ed95912884d977f21af0e8bfa797e38f38
kwondo1017 <127649467+kwondo1017@users.noreply.github.com>
2026-02-06 20:02:48 +0900

    init

---
+++ src/test/java/com/example/freshkitchen/FreshkitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freshkitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshkitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
